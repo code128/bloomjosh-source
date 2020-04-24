a709ef1bb914fb749c10c5c2138e6e9cfb52aa02
Joshua Bloom <joshbloom@google.com>
2020-04-24 10:35:01 -0700

    Commented the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 # cp the build over to the Public folder of the Firebase folder
 cp -r _site/* ~/repos/josh/fb-bloomjosh/public
+# Move to the firebase folder
 cd ~/repos/josh/fb-bloomjosh
+# Run the Deploy command
 firebase deploy
